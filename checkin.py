d39807e5b2576707cc57b9475522d402b2cf560a
priyanka <9priy8nka@gmal.com>
2024-01-11 21:54:51 +0530

    Initial commit

---
+++ checkin.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome()
+driver.implicitly_wait(10)
+driver.maximize_window()
+driver.get("https://people.zoho.com/appzlogic/zp#home/dashboard")
+driver.find_element(By.LINK_TEXT,"SIGN IN").click()
+driver.find_element(By.ID,"login_id").send_keys("priyanka.shivhare@appzlogic.com")
+driver.find_element(By.ID,"nextbtn").click()
+time.sleep(20)
+driver.find_element(By.ID,"password").send_keys("Neon@#321")
+driver.find_element(By.ID,"nextbtn").click()
+time.sleep(20)
+driver.get("https://people.zoho.com/appzlogic/zp#home/dashboard")
+time.sleep(20)
+# driver.find_element(By.XPATH,"//*[@class='in chlodIng']").click()
+driver.find_element(By.ID,"ZPD_Top_Att_Stat").click()
+time.sleep(20)
+driver.get_screenshot_as_file("file1.png")
+driver.quit()
+
+
+
